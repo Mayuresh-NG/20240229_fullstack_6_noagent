28b91030c5387ba473432825648db665e6f2b8ef
Atharva <atharvanhsm@gmail.com>
2024-03-06 17:08:52 +0530

    propertylist added

--- noagent/src/app/app.routes.ts
+++ noagent/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { MyPropertiesComponent } from './components/my-properties/my-properties.component';
 import { MyWishlistComponent } from './components/my-wishlist/my-wishlist.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
     {path:'properties',component:MyPropertiesComponent},
     {path:'wishlist',component:MyWishlistComponent},
+    {path:'propertylist', component:PropertyListComponent}
 ];
