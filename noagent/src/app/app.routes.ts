4220ac0e9340546f40dca7110273787fbf4934d5
Atharva <atharvanhsm@gmail.com>
2024-03-06 17:03:45 +0530

    myWishlist and assets added

--- noagent/src/app/app.routes.ts
+++ noagent/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { MyPropertiesComponent } from './components/my-properties/my-properties.component';
+import { MyWishlistComponent } from './components/my-wishlist/my-wishlist.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
+    {path:'properties',component:MyPropertiesComponent},
+    {path:'wishlist',component:MyWishlistComponent},
 ];
