bf820a65f2aaabb82e9755b299bf7d1ccd517fd2
Atharva <atharvanhsm@gmail.com>
2024-03-06 17:13:23 +0530

    rent-your-property added

--- noagent/src/app/app.routes.ts
+++ noagent/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { MyPropertiesComponent } from './components/my-properties/my-properties.component';
 import { MyWishlistComponent } from './components/my-wishlist/my-wishlist.component';
 import { PropertyListComponent } from './components/property-list/property-list.component';
+import { RentYourPropertyComponent } from './components/rent-your-property/rent-your-property.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
     {path:'properties',component:MyPropertiesComponent},
     {path:'wishlist',component:MyWishlistComponent},
-    {path:'propertylist', component:PropertyListComponent}
+    {path:'propertylist', component:PropertyListComponent},
+    {path:'rentyourproperty', component:RentYourPropertyComponent}
 ];
---
+++ noagent/src/app/components/rent-your-property/rent-your-property.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rent-your-property',
+  standalone: true,
+  imports: [],
+  templateUrl: './rent-your-property.component.html',
+  styleUrl: './rent-your-property.component.css'
+})
+export class RentYourPropertyComponent {
+
+}
