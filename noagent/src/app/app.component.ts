7f518478352dc1408738ff3146ab5f056b3bfab9
Sumit4482 <sbanwakde4482@gmail.com>
2024-03-07 01:11:06 +0530

    Added Signup Page

--- noagent/src/app/app.component.ts
+++ noagent/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [RouterOutlet, LoginComponent, SignupComponent]
 })
 export class AppComponent {
   title = 'noagent';
