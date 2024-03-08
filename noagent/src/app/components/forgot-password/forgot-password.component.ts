8b0d5218c9c6995d2dfab35813d74138e8d16fed
Atharva <atharvanhsm@gmail.com>
2024-03-08 16:00:33 +0530

    State change added!

--- noagent/src/app/components/forgot-password/forgot-password.component.ts
+++ noagent/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FormsModule, CommonModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
-  
+  email: string = '';
 
 }
