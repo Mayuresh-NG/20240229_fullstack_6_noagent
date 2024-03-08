a452666e7ed0d4c511b784d5c9cb5ad99cce53f1
Sumit4482 <sbanwakde4482@gmail.com>
2024-03-08 15:58:04 +0530

    Updated Validation Logic

--- noagent/src/app/components/signup-popup/signup-popup.component.ts
+++ noagent/src/app/components/signup-popup/signup-popup.component.ts
@@ -1,3 +1,4 @@
+// Importing necessary Angular modules and components
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormsModule, Validators } from '@angular/forms';
@@ -15,33 +16,43 @@ import { LoginPopupComponent } from '../login-popup/login-popup.component';
 })
 export class SignupPopupComponent {
   constructor(
+    // Dialog reference for popup
     public dialogRef: MatDialogRef<SignupPopupComponent>,
+    // Injecting data into the dialog
     @Inject(MAT_DIALOG_DATA) public data: any,
+    // Dialog service for opening/closing dialogs
     public dialog: MatDialog
   ) {}
 
+  // Method to close the signup popup
   closePopup(): void {
     this.dialogRef.close();
   }
+
+  // Method to show login popup
   showLoginPopup(): void {
     this.closePopup();
     const dialogRef = this.dialog.open(LoginPopupComponent, {
-      width: '300px', // Adjust the width based on your design
+      width: '300px',
     });
 
-    // Handle the popup closure
+    // Handling the closure of the login popup
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The popup was closed');
     });
   }
 
-  fullName: string = ''; 
+  // Initializing variables and form controls for user inputs
+  fullName: string = '';
   fullNameFormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(3),
     Validators.maxLength(50),
   ]);
-
+  fullNameInputFocused: boolean = false;
+  emailInputFocused: boolean = false;
+  phoneNumberInputFocused: boolean = false;
+  passwordInputFocused: boolean = false;
   email: string = '';
   phoneNumber: string = '';
   password: string = '';
