7ab026f622bb7c860d7b7952589d95d7f303d954
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-11 23:28:24 +0530

    Updating ayan branch and Adding of Swagger Documentations for API endpoints.

--- NEW_BACKEND/controllers/userController.js
+++ NEW_BACKEND/controllers/userController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 const nodemailer = require("nodemailer");
-const randomstring = require("randomString");
+const randomstring = require("randomstring");
 
 // internal imports
 const SECRET_KEY = process.env.SECRET_KEY;
