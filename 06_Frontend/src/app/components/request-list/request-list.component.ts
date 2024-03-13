059566a0a9187f8edd69b635ab81b91dbb42b4b1
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-13 11:49:36 +0530

    Data Rendering in request list.

--- 06_Frontend/src/app/components/request-list/request-list.component.ts
+++ 06_Frontend/src/app/components/request-list/request-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit  } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 import { RouterLink } from '@angular/router';
-import { RequestService } from '../../request.service';
+// import { RequestService } from '../../request.service';
+import { ActivatedRoute } from '@angular/router';
 import { Property } from '../../interface/request';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-request-list',
   standalone: true,
-  imports: [ProfileComponent,RouterLink],
+  imports: [ProfileComponent,RouterLink, CommonModule],
   templateUrl: './request-list.component.html',
   styleUrl: './request-list.component.css',
 })
@@ -18,21 +21,35 @@ import { HttpClient } from '@angular/common/http';
 export class RequestListComponent implements OnInit{
   filteredProperties: Property[] = [];
 
-  constructor(http: HttpClient) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+
+  ) {}
 
   ngOnInit() {
-    console.log('RequestListComponent initialized');
+    // console.log('RequestListComponent initialized');
     this.fetchFilteredProperties();
-    console.log('RequestListComponent ok');
+    // console.log('RequestListComponent ok');
   }
 
   fetchFilteredProperties(): void {
     const state = 'pending'; // Specify the state you want to filter by
-    // this.http.get<Property[]>('http://localhost:5200/properties?status=' + state)
-    //   .subscribe((properties: Property[]) => {
-    //     console.log('Filtered properties fetched:', properties);
-    //     this.filteredProperties = properties;
-    //   });
+    this.http.get<Property[]>('http://localhost:5200/properties')
+      .subscribe((properties: Property[]) => {
+        console.log('All properties fetched:', properties);
+  
+        // Filter properties by status
+        const filteredProperties = properties.filter(property => property.status === state);
+        console.log('Filtered properties:', filteredProperties);
+  
+        // Assign filtered properties to this.filteredProperties if any
+        if (filteredProperties.length > 0) {
+          this.filteredProperties = filteredProperties;
+        } else {
+          console.log('No properties with status "pending" found.');
+        }
+      });
   }
 
 }
