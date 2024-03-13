cf405051027e1355c4685637e8334e4f68a59f22
muditmalviya <malviyamudit7@gmail.com>
2024-03-13 12:51:24 +0530

    Structured the Request list page

--- 06_Frontend/src/app/components/request-list/request-list.component.ts
+++ 06_Frontend/src/app/components/request-list/request-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 import { RouterLink } from '@angular/router';
 // import { RequestService } from '../../request.service';
@@ -12,20 +12,20 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-request-list',
   standalone: true,
-  imports: [ProfileComponent,RouterLink, CommonModule],
+  imports: [ProfileComponent, RouterLink, CommonModule],
   templateUrl: './request-list.component.html',
   styleUrl: './request-list.component.css',
 })
 
 // export class RequestListComponent{}
-export class RequestListComponent implements OnInit{
+export class RequestListComponent implements OnInit {
   filteredProperties: Property[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient
 
-  ) {}
+  ) { }
 
   ngOnInit() {
     // console.log('RequestListComponent initialized');
@@ -38,11 +38,11 @@ export class RequestListComponent implements OnInit{
     this.http.get<Property[]>('http://localhost:5200/properties')
       .subscribe((properties: Property[]) => {
         console.log('All properties fetched:', properties);
-  
+
         // Filter properties by status
         const filteredProperties = properties.filter(property => property.status === state);
         console.log('Filtered properties:', filteredProperties);
-  
+
         // Assign filtered properties to this.filteredProperties if any
         if (filteredProperties.length > 0) {
           this.filteredProperties = filteredProperties;
