61849921b5beccd344620e7747500d41e909f163
muditmalviya <malviyamudit7@gmail.com>
2024-03-13 16:19:10 +0530

    Toggle button and function created for All properties.

--- 06_Frontend/src/app/components/property-list/property-list.component.ts
+++ 06_Frontend/src/app/components/property-list/property-list.component.ts
@@ -15,6 +15,7 @@ import { CommonModule } from '@angular/common';
 })
 export class PropertyListComponent implements OnInit {
   properties: Property[] = [];
+  selectedTradeType: string | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,11 +26,44 @@ export class PropertyListComponent implements OnInit {
     this.fetchProperties()
   }
 
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+    console.log(this.dropdownOpen);
+  }
+
+  selectTradeType(tradeType: string) {
+    if (tradeType === 'All') {
+      // Set selected trade type to null to fetch all pending properties
+      this.selectedTradeType = null;
+    } else {
+      this.selectedTradeType = tradeType;
+    }
+    this.fetchProperties();
+  }
+
   fetchProperties(): void {
     this.http.get<Property[]>('http://localhost:5200/properties')
       .subscribe((properties: Property[]) => {
         console.log('All properties fetched:', properties);
-        this.properties = properties;
+        
+        if (properties.length > 0) {
+
+          if(this.selectedTradeType === 'Rent'){
+            this.properties =properties.filter(property => property.trade_type === 'rent' )
+          }
+          else if(this.selectedTradeType === 'Sell'){
+            this.properties =properties.filter(property => property.trade_type === 'sell' )
+          }
+          else{
+            console.log(this.selectedTradeType)
+            this.properties = properties
+          }
+          
+        } else {
+          console.log('No properties with status "pending" found.');
+        }
       });
   }
 }
